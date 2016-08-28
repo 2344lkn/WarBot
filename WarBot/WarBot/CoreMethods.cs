52b74c4121efe4ea972577aa0040e586d7680aa7
2344lkn <2344lkn@gmail.com>
2016-08-28 13:33:48 -0700

    Discord.net, WarBot code and CoreMethods

---
+++ WarBot/WarBot/CoreMethods.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarBot
+{
+    class Core
+    {
+        public static void Intro()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" __      __             __________        __   ");
+            Console.WriteLine(@"/  \    /  \_____ ______\______   \ _____/  |_ ");
+            Console.WriteLine(@"\   \/\/   /\__  \ _  __ \    |  _//  _ \   __|");
+            Console.WriteLine(@" \        /  / __ \|  | \/    |   (  <_> )  |");
+            Console.WriteLine(@"  \__/\  /  (____  /__|  |______  /\____/|__|");
+            Console.WriteLine(@"       \/        \/             \/         v1");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static string HelpCmd()
+        {
+            string help = "``` Artificial Intelligence WarBot " + Program.Version + "\r\n" +
+                @" __      __             __________        __   " + "\r\n" +
+                @"/  \    /  \_____ ______\______   \ _____/  |_ " + "\r\n" +
+                @"\   \/\/   /\__  \ _  __ \    |  _//  _ \   __|" + "\r\n" +
+                @" \        /  / __ \|  | \/    |   (  <_> )  |" + "\r\n" +
+                @"  \__/\  /  (____  /__|  |______  /\____/|__|" + "\r\n" +
+                @"       \/        \/             \/         v1" + "\r\n" +
+                "\r\n" +
+                "[List of Commands] \r\n" +
+                " ------------------ \r\n" +
+                // Core
+                "[Core Commands] \r\n" +
+                " !help - Shows a list of commands. \r\n" +
+                " !about - Displays Version and Info about the bot. \r\n" +
+                " !exit - Shuts down the bot. \r\n" +
+                "\r\n" +
+                // Crypto
+                "[Crypto] \r\n" +
+                " ~ Polynomial Encryption \r\n" +
+                " !polye <message> <password> \r\n" +
+                "   Password must be 5 characters or greater. \r\n" +
+                " ~ Polynomial Decryption \r\n" +
+                " !polyd <encryptedString> <password> \r\n" +
+                "\r\n" +
+                // AI
+                "[AI] \r\n" +
+                " ~ AI Chat \r\n" +
+                " !chat on \r\n" +
+                " !chat off ```";
+
+            return help;
+        }
+
+        public static void WriteLineColoured(int startColor, int endColor, string text)
+        {
+            // Set Console Color before Write
+            if (startColor == 0)
+                Console.ForegroundColor = ConsoleColor.Red;
+            if (startColor == 1)
+                Console.ForegroundColor = ConsoleColor.Green;
+            if (startColor == 2)
+                Console.ForegroundColor = ConsoleColor.Gray;
+            if (startColor == 3)
+                Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine(text);
+
+            // Set Console Color after Write
+            if (endColor == 0)
+                Console.ForegroundColor = ConsoleColor.Red;
+            if (endColor == 1)
+                Console.ForegroundColor = ConsoleColor.Green;
+            if (endColor == 2)
+                Console.ForegroundColor = ConsoleColor.Gray;
+            if (endColor == 3)
+                Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+    }
+}
--- WarBot/WarBot/Program.cs
+++ WarBot/WarBot/Program.cs
@@ -4,13 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Discord;
+
 namespace WarBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => new Program().Start(args);
+
+        private const string AppName = "WarBot";
+        private const string AppUrl = "https://github.com/2344lkn/WarBot";
+        public const string Version = "1.1.4";
+
+        private static bool ChatOn = false;
+
+        private DiscordClient _bot;
+
+        private void Start(string[] args)
         {
-            //
+            Console.Title = $"{AppName} {Version}";
+
+            Core.Intro();
+
+            _bot = new DiscordClient(x =>
+            {
+                x.AppName = AppName;
+                //x.MessageCacheSize = 10;
+                //x.EnablePreUpdateEvents = true;
+            });
+
+            _bot.MessageReceived += (s, e) =>
+            {
+                #region Bot Commands
+                // Bot Command Prefix
+                if (e.Message.Text.StartsWith("!"))
+                {
+                    if (e.Message.IsAuthor)
+                    {
+                        Console.WriteLine("[BOT] " + e.Message.Text);
+                        // Do Nothing
+                    }
+                    else
+                    {
+                        if (e.Message.Text == "!help")
+                        {
+                            Core.WriteLineColoured(3, 2, e.User.ToString() + " [CMD] " + e.Message.Text);
+
+                            e.Channel.SendMessage(Core.HelpCmd());
+                        }
+                    }
+                }
+                else
+                {
+                    // NCalc
+                    // AI Chat
+                }
+                #endregion
+                //Console.WriteLine(e.User.ToString());
+            };
+
+            // Needs to be last, nothing will run after ExecuteAndWait
+            _bot.ExecuteAndWait(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        await _bot.Connect("bot.token");
+
+                        Console.WriteLine("[Connected] " + $"Connected as {_bot.CurrentUser.Name} (Id {_bot.CurrentUser.Id})");
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Attempting to reconnect...");
+                        await Task.Delay(3000);
+                    }
+                }
+            });
         }
     }
 }
